9a47aa33312c3f68f013f1b67ea506a0afd0ce63
Björn Kettunen <bjorn.kettunen@thaodan.de>
2026-08-15 13:40:13 +0300

    Fix quoting in ccls--show-xrefs "ccls.xref"

--- ccls.el
+++ ccls.el
@@ -149,7 +149,7 @@ DIRECTION can be \"D\", \"L\", \"R\" or \"U\"."
 
 (lsp-defun ccls--show-xrefs ((&Command :command :arguments?))
   (when-let ((xrefs (lsp--locations-to-xref-items
-                     (lsp--send-execute-command ccls.xref arguments?))))
+                     (lsp--send-execute-command "ccls.xref" arguments?))))
     (lsp-show-xrefs xrefs nil t)))
 
 (advice-add 'lsp--suggest-project-root :before-until #'ccls--suggest-project-root)
